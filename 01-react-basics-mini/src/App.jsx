5bb3dcc67f60477b5d4a98045e5e1880195b37e0
Beyza Nur Yilmaz <24370601015@sub.edu.tr>
2026-03-01 22:37:31 +0300

    projeyi bitirdim

--- 01-react-basics-mini/src/App.jsx
+++ 01-react-basics-mini/src/App.jsx
@@ -1,27 +1,27 @@
 import { useState,useEffect} from 'react'
 import FilmItem from './FilmItem';
 function App(){
- const [filmAdi,SetFilmAdi]=useState("");
- const [filmLİstesi,SetFilmlistesi]=useState([]);
+ const [filmAdi,setFilmAdi]=useState("");
+ const [filmLİstesi,setFilmlistesi]=useState([]);
 
  useEffect(()=>{
   alert("film sitesine hoşgeldiniz");
- },[]);
+ },[]);    //sonuna eklenen köşeli parantez sayfa ilk açılınca sadece çalısır mesaj 
 
  const FilmEkle=()=>{
   if(filmAdi!==""){
-    SetFilmlistesi([...filmLİstesi,filmAdi]);
-  SetFilmAdi("");
+    setFilmlistesi([...filmLİstesi,filmAdi]);  //yeni film ismini ekliyoruz listeye 
+  setFilmAdi("");   
   } }
  const FilmSil=(index)=>{
   const yeniFilmListesi=filmLİstesi.filter((_,i)=>i!==index)
-  SetFilmlistesi(yeniFilmListesi);
+  setFilmlistesi(yeniFilmListesi);
 }
 
   return(
     <div className='app' >
       <h1>Film listesi</h1>
-      <input type="text" placeholder='film arayın' value={filmAdi}  onChange={(e)=>SetFilmAdi(e.target.value)}/>
+      <input type="text" placeholder='film arayın' value={filmAdi}  onChange={(e)=>setFilmAdi(e.target.value)}/>
       <button onClick={FilmEkle}>Ekle</button>
       <ul>
       {filmLİstesi.length===0? <p>henüz film yok</p>: <p>{filmLİstesi.length}film var</p>}
