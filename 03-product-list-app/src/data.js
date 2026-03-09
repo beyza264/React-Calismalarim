0ff5c7ae5549d49787f5816133ee55ef69f0980c
Beyza Nur Yilmaz <24370601015@sub.edu.tr>
2026-03-09 16:06:06 +0300

    projem bitti

---
+++ 03-product-list-app/src/data.js
@@ -0,0 +1,44 @@
+export const productsData = [
+    {
+        id:1,
+        name:"iphone 14",
+        category:"elektronik",
+        price:50000,
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0dnt7VlViy2YPsH_eWOwl3iOwXpqiECrtTQ&s",
+        inStock:true
+    },
+    {
+        id:2,
+        name:"samsung s23",
+        category:"elektronik",  
+        price:45000,
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQookfQImTWOCm_XHyJo8rWUTVBet4B6_SBGQ&s",
+        inStock:false
+    
+       },
+
+       {
+        id:3,
+        name:"Sırt Çantası",
+        category:"Aksesuar",  
+        price:1000,
+        image:"https://akn-ecrou.a-cdn.akinoncloud.com/products/2024/11/29/74644/ed8ebcfc-971f-4604-a922-aa73a6e5e03e_size1340x2000_cropTop.jpg",
+        inStock:true
+       },
+       {
+        id:4,
+        name:"tişört",
+        category:"Giyim",  
+        price:25000,
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtiXFNCHxoFqni-I4GpitRePK3qLsvGW2ZHA&s",
+        inStock:true
+       },
+       {
+        id:5,
+        name:"kahve makinesi",
+        category:"Mutfak",
+        price:100000,
+        image:"https://www.senetsepet.com/idea/jz/77/myassets/products/319/202308219.jpg?revision=1716578950",
+        inStock:false
+       }
+];
